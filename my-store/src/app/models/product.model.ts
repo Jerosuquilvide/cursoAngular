0c6d20342f5e1459054272c14ba9c12c02f8a9f7
Jeronimo <jeronimosuquilvide@gmail.com>
2023-01-13 14:47:47 -0300

    Modulo HTTP & Pegando al API 'FakeStore'

--- my-store/src/app/models/product.model.ts
+++ my-store/src/app/models/product.model.ts
@@ -1,6 +1,8 @@
 export interface Product {
-    id: string;
-    name: string;
+    id: number;
+    title: string;
     image: string;
     price: number;
+    description: string;
+    category: string;
 }
