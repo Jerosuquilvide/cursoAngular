763d2958739ca21d8fbdfd025cb5cee686f000ec
Jeronimo <jeronimosuquilvide@gmail.com>
2023-01-17 10:35:15 -0300

    Solicitudes PUT y PATCH

--- angular-APIS-main/src/app/components/products/products.component.ts
+++ angular-APIS-main/src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product, CreateProductDTO } from '../../models/product.model';
+import { Product, CreateProductDTO, UpdateProductDTO } from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
@@ -73,4 +73,15 @@ export class ProductsComponent implements OnInit {
     
   }
 
+  updateProduct(){
+    const changes : UpdateProductDTO = {
+      title : 'New title'
+    }
+    const id = this.productChosen.id;
+    this.productsService.update(id,changes)
+    .subscribe(data => {
+      const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
+      this.products[productIndex] = data;
+    })
+  }
 }
